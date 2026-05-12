2f29925b030d157413f47772a034309fb4147e7d
Filip Novaković <tvoj@email.com>
2026-05-12 14:10:32 +0200

    Ispravka strukture

---
+++ GalaxyPPG.Service/Program.cs
@@ -0,0 +1,27 @@
+﻿using GalaxyPPG.Common;
+using System;
+using System.ServiceModel;
+
+namespace GalaxyPPG.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(GalaxyPPGService));
+
+            try
+            {
+                host.Open();
+                Console.WriteLine("[SERVER] Servis je pokrenut. Pritisnite Enter za zaustavljanje...");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[SERVER] Greska: {ex.Message}");
+                host.Abort();
+            }
+        }
+    }
+}
