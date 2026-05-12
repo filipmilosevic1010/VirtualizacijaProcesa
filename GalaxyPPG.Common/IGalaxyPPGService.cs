d533ea565a0d97f042df492b18c223d34ec14a28
Filip Novaković <tvoj@email.com>
2026-05-12 13:51:04 +0200

    Zadaci 1-3

---
+++ GalaxyPPG.Common/IGalaxyPPGService.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace GalaxyPPG.Common
+{
+    [ServiceContract]
+    public interface IGalaxyPPGService
+    {
+        [OperationContract]
+        void StartSession(SessionMeta meta);
+
+        [OperationContract]
+        void PushSample(EcgSample sample);
+
+        [OperationContract]
+        void EndSession();
+    }
+
+    [DataContract]
+    public class SessionMeta
+    {
+        [DataMember] public string ParticipantId { get; set; }
+        [DataMember] public string DeviceId { get; set; }
+        [DataMember] public double SampleRateHz { get; set; }
+        [DataMember] public long TimestampOffsetMs { get; set; }
+    }
+
+    [DataContract]
+    public class EcgSample
+    {
+        [DataMember] public long TimestampMs { get; set; }
+        [DataMember] public double? EcgMicroV { get; set; }
+        [DataMember] public double? HeartRate { get; set; }
+        [DataMember] public double? IBI_ms { get; set; }
+        [DataMember] public double? AccX { get; set; }
+        [DataMember] public double? AccY { get; set; }
+        [DataMember] public double? AccZ { get; set; }
+        [DataMember] public string ParticipantId { get; set; }
+        [DataMember] public int RowIndex { get; set; }
+    }
+
+    [DataContract]
+    public class DataFormatFault
+    {
+        [DataMember] public string Message { get; set; }
+    }
+
+    [DataContract]
+    public class ValidationFault
+    {
+        [DataMember] public string Message { get; set; }
+    }
+}
